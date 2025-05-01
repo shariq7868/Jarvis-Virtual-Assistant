0fcfe2fe1abcf50b33899a4daeb889dcad43df47
shariq7868 <shariqbanvasi7868@gmail.com>
2025-05-01 10:53:32 +0530

    Add Songs

---
+++ main.py
@@ -0,0 +1,70 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+from google import genai
+ 
+recogniser = sr.Recognizer()
+engine = pyttsx3.init()
+newsapi = "8a908052d68e4e97870092fe628aaf10"
+geminiapi = "AIzaSyA6i4ls4R7OFlb9RTkXB9ROlcX7HyM2mI0"
+
+def speak(text):
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id) # 0 index for male and 1,2 for female
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https//youtube.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
+        data = r.json()
+        articles = data.get("articles",[])
+        for article in articles:
+            print(article["title"])
+            speak(article["title"])
+    else:
+        client = genai.Client(api_key=f"{geminiapi}")
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=c,
+        )
+
+        print(response.text)
+        speak(response.text)
+
+
+if(__name__=="__main__"):
+    speak("Initialising jarvis....")
+    while True:
+        r = sr.Recognizer()
+
+        print("Recognizing...")
+
+        try:
+            with sr.Microphone() as source:
+                print("Listening....")
+                audio = r.listen(source,timeout=2,phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("yes")
+
+                with sr.Microphone() as source:
+                    print("jarvis Active....")
+                    audio = r.listen(source)
+                command = r.recognize_google(audio)
+                print(command)
+                processCommand(command)
+                    
+        except Exception as e:
+            print("Error;",e)
---
+++ musicLibrary.py
@@ -0,0 +1,4 @@
+music={
+    "jhol" : "https://www.youtube.com/watch?v=-2RAq5o5pwc&pp=ygUEamhvbA%3D%3D",
+    "finding her" : "https://www.youtube.com/watch?v=3Cp2QTBZAFQ&pp=ygULZmluZGluZyBoZXI%3D"
+}
